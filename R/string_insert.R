fe5073a3e38d7b3d898c0fa3063f8ed084314deb
Ke Dai <foreversuperdk@163.com>
2016-12-03 11:32:56 -0800

    Define string_insert function

---
+++ R/string_insert.R
@@ -0,0 +1,27 @@
+string_insert <- function(a, b, pos) {
+  stopifnot(class(a) == "character" && class(b) == "character")
+  stopifnot(length(a) == 1 && length(b) == 1)
+
+  if (class(a) != "character" || class(b) != "character") {
+    stop("The class of `a` and `b` must be character")
+  }
+
+  if (length(a) != 1 || length(b) != 1) {
+    stop("`a` and `b` must be a single string")
+  }
+
+  if (class(pos) != "integer") {
+    stop("The class of `pos` must be integer")
+  }
+
+  if (pos > stringr::str_length(a)) {
+    stop("pos must be less than or equal to the number of characters in a")
+  }
+
+  formerSub <- stringr::str_sub(a, 1, pos)
+  latterSub <- stringr::str_sub(a, pos + 1, stringr::str_length(a))
+
+  c <- stringr::str_c(formerSub, b, latterSub)
+
+  return(c)
+}
