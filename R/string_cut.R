b8a0ce9c7be14459699fc080716eedabb1c824d1
Ke Dai <foreversuperdk@163.com>
2016-12-03 12:28:58 -0800

    Define string_cut function

---
+++ R/string_cut.R
@@ -0,0 +1,36 @@
+string_cut <- function(a, startPos, endPos) {
+  if (class(a) != "character") {
+    stop("The class of `a` must be character")
+  }
+
+  if (length(a) != 1) {
+    stop("`a` must be a single string")
+  }
+
+  if (class(startPos) != "integer" || startPos < 1) {
+    stop("`startPos` must be a positive integer")
+  }
+
+  if (startPos > stringr::str_length(a)) {
+    stop("startPos must be less than or equal to the number of characters in a")
+  }
+
+  if (class(endPos) != "integer" || endPos < 1) {
+    stop("`endPos` must be a positive integer")
+  }
+
+  if (endPos > stringr::str_length(a)) {
+    stop("endPos must be less than or equal to the number of characters in a")
+  }
+
+  if (startPos > endPos) {
+    stop("startPos must be less than or equal to endPos")
+  }
+
+  formerSub <- stringr::str_sub(a, 1, startPos - 1)
+  latterSub <- stringr::str_sub(a, endPos + 1, stringr::str_length(a))
+
+  c <- stringr::str_c(formerSub, latterSub)
+
+  return(c)
+}
