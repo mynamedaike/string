62466307cb787b0970181520aae7aaf9598028e5
Ke Dai <foreversuperdk@163.com>
2016-12-03 21:05:55 -0800

    Document the package as whole

---
+++ R/string-package.r
@@ -0,0 +1,11 @@
+#' string: Make String Processing Easier
+#'
+#' This package is complementary to stringr package. It includes thress
+#' functions: string_insert, string_cut and string_replace. string_insert
+#' inserts a string to another string at a given position. string_cut cuts a
+#' substring between two given positions from a string. string_replace replaces
+#' a substring between two given positions in a string with another string.
+#'
+#' @name string
+#' @docType package
+NULL
